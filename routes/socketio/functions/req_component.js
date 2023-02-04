985c180a74e6b9bd467cde792d409086bd47bbd3
EliasBinder <ebinder@unibz.it>
2023-02-04 13:27:42 +0100

    Add detection to update cache in frontend if config file has been modified

--- routes/socketio/functions/req_component.js
+++ routes/socketio/functions/req_component.js
@@ -2,6 +2,8 @@ const renderer = require("../../../util/renderer");
 const cacheMgr = require("../../../util/cacheMgr");
 const {checkForParams} = require("../util");
 const {parseLanguagesField, lastChanged} = require("../../../util/languageParser");
+const fs = require("fs");
+const path = require("path");
 const handle = (socket, msg) => {
     if (!checkForParams(msg, ['type', 'name', 'languages', 'lastModified', 'reqId']))
         return;
@@ -13,8 +15,9 @@ const handle = (socket, msg) => {
     //Check language if modified
     const langToUse = parseLanguagesField(msg.languages);
     const langLastModified = lastChanged(langToUse);
-    //TODO: Check config if modified
-    if (lastModifiedComponent === msg.lastModified && msg.lastModified >= langLastModified) {
+    //Check config if modified
+    const configLastModified = fs.statSync(path.join(__dirname, '..', '..', '..', 'app.json')).mtime;
+    if (lastModifiedComponent === msg.lastModified && msg.lastModified >= langLastModified && msg.lastModified >= configLastModified) {
         socket.emit('resp', {
             reqId: msg.reqId,
             notModified: true
